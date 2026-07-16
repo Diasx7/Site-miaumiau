372b0f08120385a2f28b975cf0f82ce46cae8b21
Joao Pablo Souza Dias <joaopablosouzadias79@gmail.com>
2026-07-16 10:44:06 -0300

    config do supabase

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
-// configuracao do supabase - troca pelos dados do SEU projeto
-// pega esses dois valores em: supabase.com > seu projeto > Project Settings > API
-
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA-CHAVE-ANON-AQUI";
-
+const SUPABASE_URL = "https://ueapoofzasucarsjbrnt.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_qET28SYVV6bRrBivxlEzQA_u6LMtvqr";
